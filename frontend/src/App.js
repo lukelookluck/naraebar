dbbe3f918121c26ec91fca03016c9fcf281dda6a
lymchgmk <lymchgmk@gmail.com>
2020-07-22 16:55:51 +0900

    FRONTEND | code 'MainDisplay/Carousel'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Choiced_recipe from './pages/Choiced_recipe'
+// react
+import React, { useState } from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-body">
-        <Choiced_recipe />
-      </header>
-    </div>
+// ui
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+
+// hook
+import { CommonContext } from './context/CommonContext';
+import { useLocalStorageSetState } from './common/CommonHooks';
+
+// page
+
+import MainDisplay from './pages/MainDisplay';
+
+// css
+// import './index.css';
+
+/// theme
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ['Noto Sans KR'].join(','),
+    button: {
+      fontFamily: 'Noto Sans KR',
+    },
+    body1: {
+      fontWeight: 500,
+    },
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        body: {
+          backgroundColor: 'white',
+        },
+      },
+    },
+  },
+});
+
+// app
+const App = () => {
+  const [user, setUser] = useLocalStorageSetState(
+    {
+      user_no: 0,
+      user_id: '',
+      user_nm: '',
+      user_pwd: '',
+      user_img_url: '',
+      status: '',
+      web_site: '',
+      token: '',
+    },
+    'user',
   );
-}
+  const [infoData, setInfoData] = useState({});
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [userDialogIndex, setUserDialogIndex] = useState(0);
+  const [isShowKeyborad, setIsShowKeyborad] = useState(false);
+  const [signDialogOpen, setSignDialogOpen] = useState(false);
+  const [infoDialogOpen, setInfoDetailDialogOpen] = useState(false);
+  const [userDialogOpen, setUserDetailDialogOpen] = useState(false);
+
+  return (
+    <CommonContext.Provider
+      value={{
+        user,
+        setUser,
+        drawerOpen,
+        setDrawerOpen,
+        signDialogOpen,
+        setSignDialogOpen,
+        infoDialogOpen,
+        setInfoDetailDialogOpen,
+        infoData,
+        setInfoData,
+        userDialogOpen,
+        setUserDetailDialogOpen,
+        userDialogIndex,
+        setUserDialogIndex,
+        isShowKeyborad,
+        setIsShowKeyborad,
+      }}
+    >
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={MainDisplay} />
+            <Redirect to="/not-found" />
+          </Switch>
+        </BrowserRouter>
+      </MuiThemeProvider>
+    </CommonContext.Provider>
+  );
+};
 
 export default App;
---
+++ frontend/src/pages/MainDisplay/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { render } from 'react-dom';
+import Carousel from './Carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+const MainDisplay = () => (
+  <div>
+    <Carousel />
+  </div>
+);
+
+// render(<MainDisplay />, document.getElementById('root'));
+
+export default MainDisplay;
