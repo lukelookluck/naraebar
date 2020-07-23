704f753c8fa511e02a531944c1a3eb8bf20e4b1b
lymchgmk <lymchgmk@gmail.com>
2020-07-23 17:19:24 +0900

    FRONTEND | update src/pages/MenuList

--- frontend/src/pages/MenuList/Carousel.jsx
+++ frontend/src/pages/MenuList/Carousel.jsx
@@ -1,19 +1,49 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
+// import NotFound from '../NotFound/';
+
+// let selectedCocktail = e => {
+//   this.setState({});
+// };
 
 export default () => (
   <Carousel>
     <div>
-      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" />
-      <p className="legend">Legend 1</p>
+      <Link to="/SelectCocktail">
+        <p className="cocktail">cocktail 1</p>
+      </Link>
+      <button>
+        <img
+          alt="cocktail 1 image"
+          src="http://lorempixel.com/output/abstract-q-c-640-480-1.jpg"
+        />
+      </button>
     </div>
+
     <div>
-      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg" />
-      <p className="legend">Legend 2</p>
+      <Link to="/SelectCocktail">
+        <p className="cocktail">cocktail 2</p>
+      </Link>
+      <button>
+        <img
+          alt="cocktail 2 image"
+          src="http://lorempixel.com/output/abstract-q-c-640-480-2.jpg"
+        />
+      </button>
     </div>
+
     <div>
-      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-3.jpg" />
-      <p className="legend">Legend 3</p>
+      <Link to="/SelectCocktail">
+        <p className="cocktail">cocktail 3</p>
+      </Link>
+      <button>
+        <img
+          alt="cocktail 3 image"
+          src="http://lorempixel.com/output/abstract-q-c-640-480-3.jpg"
+        />
+      </button>
     </div>
   </Carousel>
 );
