be079228e93666e51b5ff3528027b13c6bb61c8a
이윤민 <dbsalsdl5@naver.com>
2020-07-23 00:30:57 +0000

    Upload New File

---
+++ frontend/src/pages/Complete/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Layout from '../../layout/'
+import './style.css'
+
+const Complete = () => {
+  return (
+    <Layout>
+        <div className="container">
+            <div className="bartender">
+                <img src="images/bartender2.gif"></img>
+            </div>
+            <h1>Take it and<br></br>Enjoy your drink!</h1>
+        </div>
+    </Layout>
+  );
+};
+
+export default Complete;
