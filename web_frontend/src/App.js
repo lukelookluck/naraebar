ffb5182156502c62bd1ce2529dc184b19684efdd
gunhee9177 <gunhee9177@naver.com>
2020-07-27 16:55:56 +0900

    1?

--- web_frontend/src/App.js
+++ web_frontend/src/App.js
@@ -1,19 +1,11 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-<<<<<<< HEAD
-import Main from "./pages/Main/";
-import Community from "./pages/Community/";
-import MyBar from "./pages/MyBar/";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-=======
 import Main from './pages/Main/';
 import Community from './pages/Community/';
 import MyBar from './pages/MyBar/'
 import SignUp from './pages/SignUp/'
 import FindPW from './pages/FindPW/'
->>>>>>> c52f69afcbf729d269b287d98c6bee2f5750f9e9
 
 function App() {
   return (
--- web_frontend/src/components/NavigationMenu/index.jsx
+++ web_frontend/src/components/NavigationMenu/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom';
 import './style.css'
 
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
@@ -6,15 +7,22 @@ import ChatIcon from '@material-ui/icons/Chat';
 import SearchIcon from '@material-ui/icons/Search';
 
 export default function() {
+  let history = useHistory();
+
+  const onClickRedirectPathHandler = name => e => {
+      window.scrollTo(0, 0);
+      history.push(name);
+  }
+
   return (
     <div className="nav-box">
-      <button className="nav-item">
+      <button className="nav-item" onClick={onClickRedirectPathHandler('/Community')}>
         <ChatIcon className="community-icon" />
       </button>
-      <button className="nav-item">
+      <button className="nav-item" onClick={onClickRedirectPathHandler('/MyBar')}>
         <PersonOutlineIcon className="my-bar-icon" />
       </button>
-      <button className="nav-item">
+      <button className="nav-item" >
         <SearchIcon className="search-icon" />
       </button>
     </div>
