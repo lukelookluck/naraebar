e344c3efea3f781617492f0b646051857bc51f6d
lymchgmk <lymchgmk@gmail.com>
2020-07-28 16:43:35 +0900

    FRONTEND | add Wrapper for pages/Community

--- web_frontend/src/pages/Community/index.jsx
+++ web_frontend/src/pages/Community/index.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import Layout from '../../layout/';
-import './style.css'
+import React from "react";
+import Layout from "../../layout/";
+import "./style.css";
 
-import Logo from '../../components/Logo/'
-import Nav from '../../components/NavigationMenu/'
-import Recipe from '../../components/RecipeList/'
+import Logo from "../../components/Logo/";
+import Nav from "../../components/NavigationMenu/";
+import Recipe from "../../components/RecipeList/";
+
+import Wrapper from "./style_wrapper";
 
 const Community = () => {
   return (
     <Layout>
-      <Logo />
-      <Nav />
-      <Recipe />
+      <Wrapper>
+        <Logo />
+        <Nav />
+        <Recipe />
+      </Wrapper>
     </Layout>
   );
 };
---
+++ web_frontend/src/pages/Community/style_wrapper.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  padding: 120px 0 100px;
+  text-align: center;
+  box-sizing: border-box;
+  max-width: 1280px;
+  width: 100%;
+  margin: 0 auto;
+`;
+
+export default Wrapper;
