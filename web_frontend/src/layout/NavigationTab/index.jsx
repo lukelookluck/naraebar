18c2d11a6d48207eec02c781b32a1d0aa39a0f13
이윤민 <dbsalsdl5@naver.com>
2020-07-28 23:14:35 +0900

    탭 수정중

--- web_frontend/src/layout/NavigationTab/index.jsx
+++ web_frontend/src/layout/NavigationTab/index.jsx
@@ -1,119 +1,30 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
+import { useHistory, Link } from 'react-router-dom';
+import Wrapper from "./styles";
+import { Grid } from "@material-ui/core";
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import AppBar from '@material-ui/core/AppBar';
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+const NavigationTab = () => {
+    const [value, setValue] = React.useState(0);
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`nav-tabpanel-${index}`}
-      aria-labelledby={`nav-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
+    return (
+        <Wrapper>
+            <Grid container justify="center" alignItems="center">
+                <Grid item xs={10}>
+                    <Tabs variant="fullWidth" indicatorColor="primary" textColor="primary" className="navTabs" value={value} onChange={handleChange} >
+                        <Tab className="myTab" label="커뮤니티" component={Link} to="/Community"></Tab>
+                        <Tab className="myTab" label="마이바" component={Link} to="/MyBar"></Tab>
+                    </Tabs>
+                </Grid>
+            </Grid>
+        </Wrapper>
+    );
 };
 
-function a11yProps(index) {
-  return {
-    id: `nav-tab-${index}`,
-    'aria-controls': `nav-tabpanel-${index}`,
-  };
-}
-
-function LinkTab(props) {
-  return (
-    <Tab
-      component="a"
-      onClick={(event) => {
-        event.preventDefault();
-      }}
-      {...props}
-    />
-  );
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
-
-export default function NavigationTab() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Tabs
-          variant="fullWidth"
-          value={value}
-          onChange={handleChange}
-          aria-label="nav tabs example"
-        >
-          <LinkTab label="Page One" href="/drafts" {...a11yProps(0)} />
-          <LinkTab label="Page Two" href="/trash" {...a11yProps(1)} />
-          <LinkTab label="Page Three" href="/spam" {...a11yProps(2)} />
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
-        Page One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Page Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Page Three
-      </TabPanel>
-    </div>
-  );
-}
-
-// import React from 'react';
-// import { useHistory } from 'react-router-dom';
-// import Wrapper from "./styles";
-// import AppBar from '@material-ui/core/AppBar';
-// import Tabs from '@material-ui/core/Tabs';
-// import Tab from '@material-ui/core/Tab';
-
-// const NavigationTab = () => {
-//     let history = useHistory();
-
-//     const onClickRedirectPathHandler = name => e => {
-//         window.scrollTo(0, 0);
-//         history.push(name);
-//     };
-
-//     return (
-//         <Wrapper>
-            
-//         </Wrapper>
-//     );
-// };
-
-// export default NavigationTab;
+export default NavigationTab;
--- web_frontend/src/layout/NavigationTab/styles.jsx
+++ web_frontend/src/layout/NavigationTab/styles.jsx
@@ -2,6 +2,18 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
     text-align: center;
+    & .myAppbar {
+        background-color: rgb(255, 200, 200);
+    }
+
+    & .navTabs {
+        
+    }
+
+    & .myTab {
+        font-size: 20px;
+        height: 50px;
+    }
 
 `;
 
