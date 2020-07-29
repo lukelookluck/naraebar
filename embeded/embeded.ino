07a2adaccbd3cbb82a04405b830763c4a2699ff4
황신실 <ohgyosin@naver.com>
2020-07-29 17:32:23 +0900

    modified embeded file

--- embeded/embeded.ino
+++ embeded/embeded.ino
@@ -1,7 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
+  pinMode(9, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  digitalWrite(9, LOW);   // Turn the LED
+  delay(1000);    // Wait for a seconds
+  digitalWrite(9, HIGH);  // Turn the LED
+  delay(1000);    // Wait for a seconds
 }
