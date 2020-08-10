2efdd5965e1991f942c05392b0b264e93fdc1aac
홍세진 <hongsj36@naver.com>
2020-08-10 14:52:56 +0900

    CommonContext token

--- web_frontend/src/pages/Start/auth.jsx
+++ web_frontend/src/pages/Start/auth.jsx
@@ -1,12 +1,37 @@
-const users = [
-    { id: "test", pw: "123", name: "Kim" },
-    { id: "admin", pw: "456", name: "Lee" },
-]
+import React, { useContext, useState, useEffect, useRef } from "react";
+import Axios from "axios";
+import { CommonContext } from "../../context/CommonContext";
 
-export function signIn({ id, pw }) {
-    const user = users.find(
-        (user) => user.id === id && user.pw === pw
-    )
-    if (user === undefined) throw new Error()
-    return user
+const users = [
+  { id: "test", pw: "123", name: "Kim" },
+  { id: "admin", pw: "456", name: "Lee" },
+];
+
+export function Login({ id, pw }) {
+  //     const user = users.find(
+  //         (user) => user.id === id && user.pw === pw
+  //     )
+  //     if (user === undefined) throw new Error()
+  //     return user
+
+  const { serverUrl, user, setUser } = useContext(CommonContext);
+  const url = `${serverUrl}/accounts/login/`;
+  const data = {
+    username: id,
+    password: pw,
+  };
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  console.log("로그인2");
+  Axios.post(url, data, headers)
+    .then((response) => {
+      console.log(response);
+      console.log(response.data);
+      //   setUser(...response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      throw new Error();
+    });
 }
--- web_frontend/src/pages/Start/index.jsx
+++ web_frontend/src/pages/Start/index.jsx
@@ -1,79 +1,119 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
 import { Grid, Button } from "@material-ui/core";
 import Wrapper from "./styles";
-import { signIn } from './auth';
+// import { Login } from "./auth";
 import { makeStyles } from "@material-ui/core/styles";
-import '../../index.css';
+import "../../index.css";
+
+import Axios from "axios";
+import { CommonContext } from "../../context/CommonContext";
 
 const useStyles = makeStyles({
-    logotext: {
-        fontFamily: 'Cafe24Ohsquare'
-    }
-})
+  logotext: {
+    fontFamily: "Cafe24Ohsquare",
+  },
+});
 
 const Start = () => {
+  let history = useHistory();
 
-    let history = useHistory();
+  const { serverUrl, user, setUser } = useContext(CommonContext);
 
-    const login = ({ id, pw }) => setUser(signIn({ id, pw }));
-    const [user, setUser] = useState(null);
+  const onClickRedirectPathHandler = (name) => (e) => {
+    window.scrollTo(0, 0);
+    history.push(name);
+  };
 
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
 
-    const onClickRedirectPathHandler = name => e => {
-        window.scrollTo(0, 0);
-        history.push(name);
+  const handleLogin = () => {
+    //   Login({ id, pw });
+
+    const url = `${serverUrl}/accounts/login/`;
+    const data = {
+      username: id,
+      password: pw,
     };
-
-    const [id, setId] = useState('');
-    const [pw, setPw] = useState('');
-
-    const handleLogin = () => {
-        try {
-            login({ id, pw });
-            history.push("/Main");
-            console.log("로그인 됨");
-        } catch (e) {
-            alert('아이디, 비밀번호를 확인해주세요');
-            setId('');
-            setPw('');
-        }
+    const headers = {
+      "Content-Type": "application/json",
     };
+    Axios.post(url, data, headers)
+      .then((response) => {
+        console.log(response);
+        console.log(response.data);
+        setUser({ ...response.data });
+        history.push("/Main");
+        console.log("로그인 됨");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("아이디, 비밀번호를 확인해주세요");
+        setPw("");
+      });
+  };
 
-    const classes = useStyles()
+  const classes = useStyles();
 
-    return (
-        <Wrapper>
-            <Grid container className="root" justify="center" alignItems="center">
-                <Grid item xs={12} className="logoGrid">
-                    <p className={classes.logotext} id="logo_en">NaReBar</p>
-                    <p className={classes.logotext} id="logo_ko">나만의 레시피 바텐더</p>
-                </Grid>
-                <Grid item xs={10} className="login">
-                    <div>
-                        <input id="id" type="text" name="id" placeholder="아이디"
-                            value={id} onChange={({ target: { value } }) => setId(value)}></input>
-                    </div>
-                    <div>
-                        <input id="pw" type="password" name="pw" placeholder="비밀번호"
-                            value={pw} onChange={({ target: { value } }) => setPw(value)}></input>
-                    </div>
-                    <Button variant="contained" className="login_btn" onClick={handleLogin}>로그인</Button>
-                </Grid>
-                <Grid item xs={6} className="bottom">
-                    <div id="signup" onClick={onClickRedirectPathHandler('/SignUp')}>회원가입</div>
-                </Grid>
-                <Grid item xs={6} className="bottom">
-                    <div id="findpw" onClick={onClickRedirectPathHandler('/FindPW')}>비밀번호<br/>찾기</div>
-                </Grid>
-            </Grid>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <Grid container className="root" justify="center" alignItems="center">
+        <Grid item xs={12} className="logoGrid">
+          <p className={classes.logotext} id="logo_en">
+            NaReBar
+          </p>
+          <p className={classes.logotext} id="logo_ko">
+            나만의 레시피 바텐더
+          </p>
+        </Grid>
+        <Grid item xs={10} className="login">
+          <div>
+            <input
+              id="id"
+              type="text"
+              name="id"
+              placeholder="아이디"
+              value={id}
+              onChange={({ target: { value } }) => setId(value)}
+            ></input>
+          </div>
+          <div>
+            <input
+              id="pw"
+              type="password"
+              name="pw"
+              placeholder="비밀번호"
+              value={pw}
+              onChange={({ target: { value } }) => setPw(value)}
+            ></input>
+          </div>
+          <Button
+            variant="contained"
+            className="login_btn"
+            onClick={handleLogin}
+          >
+            로그인
+          </Button>
+        </Grid>
+        <Grid item xs={6} className="bottom">
+          <div id="signup" onClick={onClickRedirectPathHandler("/SignUp")}>
+            회원가입
+          </div>
+        </Grid>
+        <Grid item xs={6} className="bottom">
+          <div id="findpw" onClick={onClickRedirectPathHandler("/FindPW")}>
+            비밀번호
+            <br />
+            찾기
+          </div>
+        </Grid>
+      </Grid>
+    </Wrapper>
+  );
 };
 
 export default Start;
-
-
 
 // class Main extends React.Component {
 
