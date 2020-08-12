27f7fdd46af137c817152b45b052b0390719602e
lymchgmk@gmail.com <lymchgmk@gmail.com>
2020-08-12 11:11:50 +0900

    DJANGO | update backend/web/recipe

--- backend/web/recipe/models.py
+++ backend/web/recipe/models.py
@@ -4,24 +4,35 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils import timezone
 
 
+# class Recipe(models.Model):
+#     objects = models.Manager()
+#     name = models.CharField(max_length=100)
+#     author = models.CharField(max_length=50)
+#     content = models.CharField(max_length=200)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#     uploaded_at = models.DateTimeField(auto_now=True)
+#     like_users = models.ManyToManyField(
+#         settings.AUTH_USER_MODEL, related_name='like_recipe_users')
+#     rating = models.IntegerField(
+#         validators=[MinValueValidator(1), MaxValueValidator(5)])
+#     daily_ranking = models.IntegerField(default=999)
+#     weekly_ranking = models.IntegerField(default=999)
+#     monthly_ranking = models.IntegerField(default=999)
+
+#     def __str__(self):
+#         return self.name
+
 class Recipe(models.Model):
-    objects = models.Manager()
     name = models.CharField(max_length=100)
-    author = models.CharField(max_length=50)
-    content = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    uploaded_at = models.DateTimeField(auto_now=True)
-    like_users = models.ManyToManyField(
-        settings.AUTH_USER_MODEL, related_name='like_recipe_users')
-    rating = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(5)])
-    daily_ranking = models.IntegerField(default=999)
-    weekly_ranking = models.IntegerField(default=999)
-    monthly_ranking = models.IntegerField(default=999)
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add="true")
+    detail = models.TextField(max_lengh=300)
+    ingredients = models.JSONField(encoder="None")
+    image = models.ImageField()
 
     def __str__(self):
-        return self.name
+        return '{}: {}'.format(self.name, self.image)
 
 
 class IngredientCategory(models.Model):
--- backend/web/recipe/views.py
+++ backend/web/recipe/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-
 from .models import Recipe, Ingredient, IngredientCategory
 from .serializers import RecipeSerializer, IngredientSerializer
-from rest_framework import generics, permissions, status
+from rest_framework import generics, mixins, permissions, status
 from rest_framework.decorators import permission_classes, authentication_classes, api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
@@ -10,23 +9,23 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 @permission_classes((IsAuthenticated,))
 @authentication_classes((JSONWebTokenAuthentication,))
-class RecipeList(generics.ListCreateAPIView):
+class RecipeList(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.ListCreateAPIView):
     queryset = Recipe.objects.all().order_by('-rating')
     serializer_class = RecipeSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
-@permission_classes((IsAuthenticated,))
-@authentication_classes((JSONWebTokenAuthentication,))
-class UploadRecipe(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Recipe.objects.all()
-    serializer_class = RecipeSerializer
-    permission_classes = [permissions.IsAuthenticated]
+# @permission_classes((IsAuthenticated,))
+# @authentication_classes((JSONWebTokenAuthentication,))
+# class UploadRecipe(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Recipe.objects.all()
+#     serializer_class = RecipeSerializer
+#     permission_classes = [IsAuthenticated]
 
 
-@permission_classes((IsAuthenticated,))
-@authentication_classes((JSONWebTokenAuthentication,))
-class DeleteRecipe(generics.RetrieveDestroyAPIView):
-    queryset = Recipe.objects.all()
-    serializer_class = RecipeSerializer
-    permission_classes = [permissions.IsAuthenticated]
+# @permission_classes((IsAuthenticated,))
+# @authentication_classes((JSONWebTokenAuthentication,))
+# class DeleteRecipe(generics.RetrieveDestroyAPIView):
+#     queryset = Recipe.objects.all()
+#     serializer_class = RecipeSerializer
+#     permission_classes = [IsAuthenticated]
