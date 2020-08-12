d73c4455343695f335c8d917ec0b21f062a1df14
lymchgmk@gmail.com <lymchgmk@gmail.com>
2020-08-12 11:33:03 +0900

    DJANGO | update backend/web/recipe

--- backend/web/recipe/urls.py
+++ backend/web/recipe/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 app_name = "recipe"
 
+# urlpatterns = [
+#     path('list/', views.RecipeList.as_view()),
+#     path('upload/', views.UploadRecipe.as_view()),
+#     path('<int:recipe_pk>/delete/', views.DeleteRecipe.as_view()),
+# ]
+
 urlpatterns = [
-    path('list/', views.RecipeList.as_view()),
-    path('upload/', views.UploadRecipe.as_view()),
-    path('<int:recipe_pk>/delete/', views.DeleteRecipe.as_view()),
+    path('', views.recipeListView.as_view()),
 ]
--- backend/web/recipe/views.py
+++ backend/web/recipe/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Recipe, Ingredient, IngredientCategory
 from .serializers import RecipeSerializer, IngredientSerializer
 from rest_framework import generics, mixins, permissions, status
@@ -9,10 +10,13 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 @permission_classes((IsAuthenticated,))
 @authentication_classes((JSONWebTokenAuthentication,))
-class RecipeList(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.ListCreateAPIView):
+class recipeListView(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.ListCreateAPIView):
+    model = Recipe
+    context_object_name = 'recipe_list'
     queryset = Recipe.objects.all().order_by('-rating')
     serializer_class = RecipeSerializer
     permission_classes = [IsAuthenticated]
+    # template_name = '#'
 
 
 # @permission_classes((IsAuthenticated,))
