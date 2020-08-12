f7a7d630a56d7e0688980fec35255a235d866369
이윤민 <dbsalsdl5@naver.com>
2020-08-12 23:55:53 +0900

    update local frontend

--- frontend/src/pages/MenuList/Carousel.jsx
+++ frontend/src/pages/MenuList/Carousel.jsx
@@ -1,28 +1,48 @@
-import React, {useState} from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Carousel } from 'react-bootstrap';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
 import Wrapper from './style';
-import dumpfile from "../dump.json";
 import { Grid } from "@material-ui/core";
+import { CommonContext } from "../../context/CommonContext";
+import axios from "axios";
 
 export default function () {
 
-  const menuDump = dumpfile;
-
   let history = useHistory();
-
   const onClickRedirectPathHandler = name => e => {
     history.push(name);
   };
-  
-  const menu = menuDump.map((item) => {
+
+
+  const [articleList, setArticleList] = useState([]);
+  const { serverUrl } = useContext(CommonContext);
+
+  function refreshList() {
+    axios
+      .get(`${serverUrl}/bartender/recipe`, {
+        // headers: {
+        //   Authorization: `JWT ${user.token}`,
+        // },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setArticleList(res.data);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    refreshList();
+  }, []);
+
+
+  const menu = articleList.map((item) => {
     return (
       <Carousel.Item key={item.id} className="caroitem" onClick={onClickRedirectPathHandler('/SelectCocktail/' + item.id)}>
-        <img src={item.img} alt={item.name} className="cocktailimg"></img>
+        <img src={item.image} alt={item.name} className="cocktailimg"></img>
         <Carousel.Caption>
           <h1>{item.name}</h1>
-          <p>{item.details}</p>
+          <p>{item.detail}</p>
         </Carousel.Caption>
       </Carousel.Item>
     );
