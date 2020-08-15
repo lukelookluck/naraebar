5ffb0eaf5170d9d44d4515aecb6c5c6b09fcb7a1
이윤민 <dbsalsdl5@naver.com>
2020-08-15 20:43:05 +0900

    update

--- frontend/src/pages/Start/styles.jsx
+++ frontend/src/pages/Start/styles.jsx
@@ -5,44 +5,52 @@ const Wrapper = styled.div`
     text-align: center;
     
     & .logo p {
-        margin-top: 180px;
+        margin-top: 150px;
         font-size: 200px;
         font-family: 'Cafe24Ohsquare';
         color: rgb(56, 97, 149);
         margin-bottom: 0px;
     }
 
-    & .logo h1 {
-        font-size: 70px;
-        font-family: 'IBMPlexSansKR-Text';
+    & .btndiv {
+        margin-top: 40px;
     }
 
     & .btnCleaning {
-        background-color: rgb(200, 200, 200);
+        background-color: rgb(140, 140, 140);
         text-decoration: none;
         border: none;
-        color: black;
+        color: white;
         font-size: 50px;
         font-family: 'IBMPlexSansKR-Text';
         height: 100px;
-        width: 250px;
-        margin-right: 30px;
+        width: 300px;
+        margin-right: 90px;
+    }
+
+    & .btnCleaning:hover {
+        background-color: rgb(140, 140, 140);
     }
 
     & .btnStart {
-        background-color: rgb(76, 127, 188);
+        background-color: rgb(76, 127, 200);
         text-decoration: none;
         border: none;
         color: white;
         font-family: 'IBMPlexSansKR-Text';
         font-size: 50px;
         height: 100px;
-        width: 250px;
-        margin-left: 30px;
+        width: 300px;
+        margin-left: 90px;
     }
 
     & .btnStart:hover {
-        background-color: rgb(76, 127, 188);
+        background-color: rgb(76, 127, 200);
+    }
+
+    & .MuiSvgIcon-root {
+        width: 50px;
+        height: 50px;
     }
 
 `
