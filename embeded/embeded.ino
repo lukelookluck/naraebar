ededf1f4bb1212f4ddfaaea1ff21b4006a1f9477
황신실 <ohgyosin@naver.com>
2020-08-18 00:18:20 +0900

    modified flowmeter code

--- embeded/embeded.ino
+++ embeded/embeded.ino
@@ -1,13 +1,15 @@
 char data = 9;
-int pulse = 0;
-float lqd = 0;
+int pulse = 0; // 유량계입력 pulse
+float lqd = 0; // 누적 용액
 int i = 0;
-int mode = 0;
-int cnt = 0;
-int timer = 1;
-int flag = 0;
-int mon = 1;
+int mode = 0; // 현재 작동 모드 -> make? wash? error?
+int cnt = 0; // ISR에서 0.1초를 만들기 위해 쌓음
+int timer = 1; // 0.1초가 되면 timer 변수가 0이 됨
+int flag = 0; // alarm 함수에서 몇초가 경과되었는지 확인용
+int mon = 1; // 현재 펌프가 on인지 off인지 확인할 수 있음
+int mon_before = 1;
 int x = 0; // switch문 사용 용도 
+int y = 0; 
 
 void setup() {
   //pinMode(6, OUTPUT); //PWM OUT
@@ -49,15 +51,23 @@ void loop() {
   //    echoSerial(data);
   //  }
   pwmTest();
-
   countFlow();
 }
+
+
+int make (){
+  //전역변수 써서 그 전역변수 idx의 bottle과 contain 의 값에 따라 음료를 만들기 
+  // 전역변수 idx의 bottle과 contain 의 값이 -1이면 return 1
+  // 1을 return 하면 다 만들었으니까 다만들었다는 신호 보내기
+  //나머지는 0 리턴 0일 경우에는 전역변수 값 1개씩 증가
+}
+
 
 // 1초동안 펄스를 측정하여 유량을 계산
 void countFlow() {
   float flow;
   int tmp = 0;
-  // 타이머카운터 인터럽트 써서 1초 되면 mo loop문 자체를 이용해서 계속 감시할 수 있음
+  // 타이머카운터 인터럽트 써서 1초 되면 loop문 자체를 이용해서 계속 감시할 수 있음
   if (timer == 0) {
     tmp = pulse;
 
@@ -87,6 +97,31 @@ void countFlow() {
       }
     }
 
+    if (mon != mon_before){
+      y = 1;
+    }
+
+    mon_before = mon;
+    
+    switch (y) {
+      case 0: // 정상 입력값 -> 그대로 누적해줌 (break)
+        break;
+      case 1: // mon의 변경이 있을 때 -> 그대로 누적해줌 (break)
+        y = 2;
+        break;
+      case 2: // mon변경 다음 -> 채터링 값을 버려줌 (flow를 빼기)
+        lqd -= flow;
+        y = 3;
+        break;
+//      case 3: // mon이 0이 되고 처음온 값 -> 1.5배로 누적해줌 (flow의 반을 더하기)
+//        lqd += (flow/2);
+//        y = 0;
+//        break;  
+      default: 
+        break;
+    }
+
+    
     timer = 1;
     pulse = 0;
   }
@@ -99,7 +134,7 @@ void pulseCount() {
 
 // 해당 초가 지나면 1을 반환 
 int alarm (int delayTime){
-  if (flag >= delayTime){
+  if (flag >= (delayTime*10)){
     flag = 0;
     return 1;
   }
@@ -117,15 +152,15 @@ void pwmTest() {
     switch(x) {
       case 0: 
         mon = HIGH;
-        // 20초가 지나면 알람이 1이 됨 
-        if (alarm(20)) {
+        // 2초가 지나면 알람이 1이 됨 
+        if (alarm(2)) {
           mon = LOW;
           x = 1;
         }
         break;
       case 1:
         mon = LOW;
-        if (alarm(20)) {
+        if (alarm(1)) {
           mon = HIGH;
           x = 2;
         }
