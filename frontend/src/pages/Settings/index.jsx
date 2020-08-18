0db8e922fd6ae621a5b62c3a6fd5437f520c13d7
이윤민 <dbsalsdl5@naver.com>
2020-08-18 19:46:53 +0900

    add Settings

---
+++ frontend/src/pages/Settings/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../../layout/';
+import Wrapper from './styles';
+
+
+const Settings = () => {
+  
+
+  return (
+    <Layout>
+      
+    </Layout>
+  );
+};
+
+export default Settings;
