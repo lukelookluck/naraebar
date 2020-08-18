fe64bb258d2883c1909813dc9c3c40542db38c91
이윤민 <dbsalsdl5@naver.com>
2020-08-18 15:02:55 +0900

    add random btn

--- frontend/src/pages/MenuList/index.jsx
+++ frontend/src/pages/MenuList/index.jsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import Carousel from './Carousel';
 import { Grid, Button } from '@material-ui/core';
 import Layout from '../../layout/';
+import Wrapper from './style';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 const MenuList = () => {
-  const [item, setItem] = useState([]);
+  // const [item, setItem] = useState([]);
 
   let history = useHistory();
 
@@ -23,7 +24,7 @@ const MenuList = () => {
       })
       .then(res => {
         console.log(res.data);
-        setItem(res.data);
+        // setItem(res.data);
         console.log('아이디는 ' + res.data.id);
         goRandomMenu('/SelectCocktail/' + res.data.id);
       })
@@ -32,10 +33,16 @@ const MenuList = () => {
 
   return (
     <Layout>
-      <Carousel />
-      <Button className="btnRandom" variant="contained" onClick={random}>
-        뭐 마시지?
-      </Button>
+      <Wrapper>
+        <Carousel />
+        <Grid container justify="center" alignItems="center">
+          <Grid item xs={12}>
+            <Button className="btnRandom" variant="contained" onClick={random}>
+              지금의 칵테일
+            </Button>
+          </Grid>
+        </Grid>
+      </Wrapper>
     </Layout>
   );
 };
