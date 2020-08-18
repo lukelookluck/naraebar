cf43d742e4fcc7d5e28b059fdb7a45f4b101c8f4
이윤민 <dbsalsdl5@naver.com>
2020-08-18 21:10:19 +0900

    update

--- frontend/src/pages/Settings/index.jsx
+++ frontend/src/pages/Settings/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import Layout from '../../layout/';
 import Wrapper from './styles';
+import {Grid} from '@material-ui/core';
 
 
 const Settings = () => {
   
-
   return (
     <Layout>
-      <div>뿌야</div>
+        <Wrapper>
+            <Grid container justify="center" alignItems="center">
+                <Grid item xs={3}>
+                    
+                </Grid>
+                <Grid item xs={3}>
+
+                </Grid>
+                <Grid item xs={3}>
+
+                </Grid>
+                <Grid item xs={3}>
+
+                </Grid>
+            </Grid>
+        </Wrapper>
     </Layout>
   );
 };
