20f01c52e7a116f74762e333d6b50e2fbcf57300
이윤민 <dbsalsdl5@naver.com>
2020-08-18 22:56:56 +0900

    update frontend settings and setBottile

---
+++ frontend/src/pages/SetBottle/index.jsx
@@ -0,0 +1,21 @@
+import React, {useEffect, useState, Fragment} from 'react';
+import Layout from '../../layout/';
+import Wrapper from './styles';
+import { Grid } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import axios from "axios";
+import ArrowBackRoundedIcon from '@material-ui/icons/ArrowBackRounded';
+
+const Setbottle = () => {
+
+    
+    return (
+        <Layout>
+            <Wrapper>
+                <div>까꿍</div>
+            </Wrapper>
+        </Layout>
+    );
+};
+
+export default Setbottle;
